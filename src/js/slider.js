0d5ecce69100b52c55bdae724f02ea5383993b37
il0naa <136170246+il0naa@users.noreply.github.com>
2023-08-13 10:29:30 +0200

    second commit

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,17 +2,18 @@ import Swiper from 'swiper/bundle';
 
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
 
     scrollbar: {
         el: ".swiper-scrollbar",
-        hide: true,
+        // hide: true,
+        behavior: 'smooth',
+        draggable: true,
       },
   });
+
+  const container = document.getElementById('container-clients-reviews');
+  const elementToScrollTo = container.querySelector('.element-to-scroll-to');
+  elementToScrollTo.scrollIntoView({ behavior: 'smooth' });
+  
+  
