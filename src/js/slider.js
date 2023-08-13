c276c77d84324d73df765ab968496fdf9fa940b7
il0naa <136170246+il0naa@users.noreply.github.com>
2023-08-13 17:01:39 +0200

    added slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -2,14 +2,26 @@ import Swiper from 'swiper/bundle';
 
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
     speed: 200,
     loop: true,
   
-    // If we need pagination
     pagination: {
       el: '.pagination',
       clickable: true,
     },
+
+    slidesPerView: 1,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1440: {
+        slidesPerView: 3,
+      }
+    }
+
   });
   
